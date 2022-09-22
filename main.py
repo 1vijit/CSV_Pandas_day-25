9be8b4f7dffe937171c66f4ef56ddca27974fb50
1vijit <111475826+1vijit@users.noreply.github.com>
2022-09-22 09:31:11 -0400

    intermediate commit

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-# with open("weather_data.csv") as file:
-#     body=file.readlines()
-#     print(body)
-
-import csv
-import re
-
-
-
-#
+import pandas
+#import csv
 # with open("weather_data.csv") as file:
 #     data = csv.reader(file)
 #     temperature = []
-#     for row in file:
-#         print(type(row))
-#         temp_x = re.search(",(.+?),", row)
-#         if temp_x.group(1) != 'temp':
-#             temperature.append(temp_x.group(1))
+#     for row in data:
+#         if row[1] != 'temp':
+#             print(row[1])
+#             temperature.append(int(row[1]))
+#
 #     print((temperature))
 
 
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    temperature = []
-    for row in data:
-        if row[1] != 'temp':
-            print(row[1])
-            temperature.append(row[1])
+data=pandas.read_csv("weather_data.csv")
+# print(data)
 
-    print((temperature))
+data_dict = data.to_dict()
+# print(data_dict)
 
+temp_list = data["temp"].to_list()
+
+mon=data[data.day == "Monday"]
+
+m_temp =9/5 *(int(mon.temp)) +32
+print(f"{m_temp} F")
+
+
+
+# create dataframe from scratch
+data=pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
